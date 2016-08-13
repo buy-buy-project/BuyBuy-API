613204daf2daa330d306ed7cb57e7da1e6d88d3d
Lucas Dourado <lucasldourado@gmail.com>
2016-08-13 23:24:48 +0000

    migration tabela lista_compra

---
+++ database/migrations/2016_08_13_225420_create_lista_compra_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateListaCompraTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lista_compra', function (Blueprint $table) {
+            $table->increments('id');
+            $table->datetime('data_lista');
+            $table->tinyInteger('recomendada')->default(0);
+            $table->tinyInteger('confirmada')->default(0);
+        });
+
+        Schema::table('lista_compra', function (Blueprint $table) {
+            $table->integer('consumidor_id')
+                ->unsigned()
+                ->nullable()
+                ->after('confirmada');
+                
+            $table->foreign('consumidor_id')->references('id')->on('consumidor');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('lista_compra', function (Blueprint $table) {
+            $table->dropForeign('lista_compra_consumidor_id_foreign');
+            $table->dropColumn('consumidor_id');            
+        });
+
+        Schema::drop('lista_compra');
+    }
+}
