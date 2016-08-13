8a370e9bd7c4f99e9a1af36ef115fd469253d10c
Lucas Dourado <lucasldourado@gmail.com>
2016-08-13 23:31:28 +0000

    migration tabela compra

---
+++ database/migrations/2016_08_13_232500_create_compra_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompraTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('compra', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('quantidade');
+        });
+
+        Schema::table('compra', function (Blueprint $table) {
+            $table->integer('lista_id')
+                ->unsigned()
+                ->after('quantidade');
+
+            $table->integer('produto_id')
+                ->unsigned()
+                ->after('quantidade');
+                
+            $table->foreign('lista_id')->references('id')->on('lista_compra');
+            $table->foreign('produto_id')->references('id')->on('produto');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('compra', function (Blueprint $table) {
+            $table->dropForeign('compra_lista_id_foreign');
+            $table->dropColumn('lista_id');
+            $table->dropForeign('compra_produto_id_foreign');
+            $table->dropColumn('produto_id');
+        });
+
+        Schema::drop('compra');
+    }
+}
