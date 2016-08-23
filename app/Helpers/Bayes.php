a6b03006914316b84312a717fb38292ddc432051
Lucas Dourado <lucasldourado@gmail.com>
2016-08-23 13:55:49 +0000

    Ajuste no calculo das probabilidades

--- app/Helpers/Bayes.php
+++ app/Helpers/Bayes.php
@@ -7,9 +7,8 @@ class Bayes
 	const QTD_DIAS = 90;
 
 	public static function inferencia($redeMarkov, $historico, $totalFuncaoTransicao) {
-		$situacoes = [];
-
 		// Gera as situações
+		$situacoes = [];
 		foreach ($historico as $t => $quantidade) {
 			if($quantidade != -1) {
 				$dt = self::QTD_DIAS - $t;
@@ -22,7 +21,11 @@ class Bayes
 		foreach($redeMarkov as $estado => $transicoes) {
 			$formula = 1;
 			foreach ($situacoes as $dt => $quantidade) {
-				$formula *= (1 - ($redeMarkov[$quantidade][$estado][$dt]/$totalFuncaoTransicao[$quantidade][$estado]) );
+				$qtdVezesTransicao = isset($redeMarkov[$quantidade][$estado][$dt]) ? $redeMarkov[$quantidade][$estado][$dt] : 0;
+
+				$qtdTotalVezesTransicao = isset($totalFuncaoTransicao[$quantidade][$estado]) ? $totalFuncaoTransicao[$quantidade][$estado] : 1;
+
+				$formula *= (1 - ($qtdVezesTransicao/$qtdTotalVezesTransicao) );
 			}
 
 			$probabilidades[$estado] = 1 - $formula;
