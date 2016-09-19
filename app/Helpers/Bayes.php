903fb3eb4fde394ee2d71915b3e035ffe336e139
Lucas Dourado <lucasldourado@gmail.com>
2016-09-18 21:10:09 -0300

    Alteração na inferencia

--- app/Helpers/Bayes.php
+++ app/Helpers/Bayes.php
@@ -47,6 +47,53 @@ class Bayes
 		return [$quantidadeMaiorProbabilidade => $maiorProbabilidade];
 	}
 
+	public static function inferenciaNova($redeMarkov, $historico, $totalFuncaoTransicao) {
+		// Gera as situações
+		$situacoes = [];
+		foreach ($historico as $t => $quantidade) {
+			if($quantidade != -1) {
+				$dt = 1 + self::QTD_DIAS - $t;
+				$situacoes[$dt] = $quantidade;
+			}
+		}
+
+		// Calcula a probabilidade das quantidades
+		$probabilidades = [];
+		foreach($redeMarkov as $estado => $transicoes) {
+			$formulaTotal = 0;
+			$formula = 0;
+			foreach ($situacoes as $dt => $quantidade) {
+				$qtdVezesTransicao = isset($redeMarkov[$quantidade][$estado][$dt]) ? $redeMarkov[$quantidade][$estado][$dt] : 0;
+				$formula += $qtdVezesTransicao;
+			}
+
+			foreach ($totalFuncaoTransicao as $estadoAtual => $proximoEstado) {
+				foreach ($proximoEstado as $prox => $valorEstado) {
+					if($prox == $estado) {
+						$formulaTotal += $proximoEstado[$estado];
+					}
+				}
+			}
+
+			$probabilidades[$estado] = $formula / $formulaTotal;
+		}
+
+		// Calcula fator de normalização
+		$somaDasProbabilidades = array_sum($probabilidades);
+		$fatorNormalizacao = 1 / $somaDasProbabilidades;
+
+		// Aplica o fator nas probabilidades
+		array_walk($probabilidades, array('self', 'aplicaFatorNormalizacao'), $fatorNormalizacao);
+
+		arsort($probabilidades);
+
+		$quantidadeMaiorProbabilidade = key($probabilidades);
+
+		$maiorProbabilidade = array_shift($probabilidades);
+
+		return [$quantidadeMaiorProbabilidade => $maiorProbabilidade];
+	}
+
 	private static function aplicaFatorNormalizacao(&$valor, $indice, $fator) {
 		$valor = ($valor * $fator) * 100;
 	}
